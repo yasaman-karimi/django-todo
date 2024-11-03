e0d97e28ed9a6955db4ea8c6818c1bbe5dd05dfb
Yasaman karimi <yasmin.kmi.ag@gmail.com>
2024-11-03 17:07:32 +0330

    Update CORS settings and dependencies

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
     "apps.user",
 ]
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -55,8 +55,15 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization",
+    "x-api-key",
+    "x-requested-with",
+]
 ROOT_URLCONF = "todo_app.urls"
 
 TEMPLATES = [
